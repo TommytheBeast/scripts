f0e0d7f5de3a4fb297447e1151f6b259a3266c7b
TommytheBeast <89943979+TommytheBeast@users.noreply.github.com>
2023-10-18 13:08:21 +0100

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -6,6 +6,7 @@ sudo apt-get dist-upgrade
 sudo apt-get install clamtk
 freshclam
 sudo apt-get install ufw
-ufw enable
-ufw status
-echo "sudo nano /etc/ssh/sshd_config |PermRootLogin no| LoginGraceTime 60| Protocol 2| PermitEmptyPasswords no| PasswordAuthentication| X11Forwarding no| UsePAM yes| UsePrivilegeSeperation yes|"
+sudo ufw enable
+sudo ufw status
+sudo nano /etc/ssh/sshd_config |PermRootLogin no| LoginGraceTime 60| Protocol 2| PermitEmptyPasswords no| PasswordAuthentication| X11Forwarding no| UsePAM yes| UsePrivilegeSeperation yes|
+sudo sed 's/PermitRootLogin yes/PermitRootLogin no/g' sshd_config
